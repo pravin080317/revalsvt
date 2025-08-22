17fa93b961a8080f91b97ff03963f228ef332c70
pravin080317 <35328578+pravin080317@users.noreply.github.com>
2025-08-22 17:32:44 +0100

    feat: replace grid with fluent UI version

--- DetailsListVOA/index.ts
+++ DetailsListVOA/index.ts
@@ -1,26 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument */
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
-import { Grid, IGridProps } from "./Grid";
+import { Grid, GridProps } from "./Grid";
 import * as React from "react";
-import { ITheme } from '@fluentui/react';
+import { IDetailsList, Selection, SelectionMode } from '@fluentui/react';
 
 export class DetailsListVOA implements ComponentFramework.ReactControl<IInputs, IOutputs> {
     private notifyOutputChanged: () => void;
 
-    /**
-     * Empty constructor.
-     */
     constructor() {
         // Empty
     }
 
-    /**
-     * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
-     * Data-set values are not initialized here, use updateView.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
-     * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
-     * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
-     */
     public init(
         context: ComponentFramework.Context<IInputs>,
         notifyOutputChanged: () => void,
@@ -29,34 +19,36 @@ export class DetailsListVOA implements ComponentFramework.ReactControl<IInputs, 
         this.notifyOutputChanged = notifyOutputChanged;
     }
 
-    /**
-     * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
-     * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
-     * @returns ReactElement root react element for the control
-     */
     public updateView(context: ComponentFramework.Context<IInputs>): React.ReactElement {
-        const theme = (context as unknown as { fluentTheme?: ITheme }).fluentTheme;
-        const props: IGridProps = {
-            name: 'Power Apps',
-            theme,
+        const selection = new Selection();
+        const componentRef = React.createRef<IDetailsList>();
+
+        const props: GridProps = {
+            datasetColumns: [],
+            columns: {},
+            sortedColumnIds: [],
+            records: {},
+            sortedRecordIds: [],
+            shimmer: false,
+            itemsLoading: false,
+            selectionType: SelectionMode.none,
+            selection,
+            onNavigate: () => undefined,
+            onCellAction: () => undefined,
+            onSort: () => undefined,
+            sorting: [],
+            componentRef,
+            selectOnFocus: false,
+            ariaLabel: null,
+            resources: context.resources,
         };
-        return React.createElement(
-            Grid, props
-        );
+        return React.createElement(Grid, props);
     }
 
-    /**
-     * It is called by the framework prior to a control receiving new data.
-     * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as "bound" or "output"
-     */
     public getOutputs(): IOutputs {
-        return { };
+        return {};
     }
 
-    /**
-     * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
-     * i.e. cancelling any pending remote calls, removing listeners, etc.
-     */
     public destroy(): void {
         // Add code to cleanup control if necessary
     }
