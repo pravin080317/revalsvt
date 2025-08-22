648b18f7c8cd8a7bd8a6add34bf0f75bbb818f88
pravin080317 <35328578+pravin080317@users.noreply.github.com>
2025-08-22 17:21:32 +0100

    refactor: address lint warnings in grid cell

--- DetailsListVOA/index.ts
+++ DetailsListVOA/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument */
 import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import { Grid, IGridProps } from "./Grid";
 import * as React from "react";
